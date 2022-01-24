37c1e8e7b85b2cc53b5d69e34646063658cb1295
Dia <balaska.klaudiaa@gmail.com>
2022-01-24 15:37:34 +0100

    Dashboard design, icons, attempt to make responsive

---
+++ src/main/java/com/example/findpho_adminui/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.findpho_adminui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
