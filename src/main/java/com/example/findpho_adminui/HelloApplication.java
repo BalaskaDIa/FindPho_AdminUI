930e27c9d47dd28939f718d2fe73da078d0c6f20
Dia <balaska.klaudiaa@gmail.com>
2022-01-26 15:14:48 +0100

    Responsivity done

--- src/main/java/com/example/findpho_adminui/HelloApplication.java
+++ src/main/java/com/example/findpho_adminui/HelloApplication.java
@@ -3,6 +3,7 @@ package com.example.findpho_adminui;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,10 +12,13 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("home.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 900, 650);
+        stage.setTitle("FindPho");
+        stage.setMinHeight(600);
+        stage.setMinWidth(800);
         stage.setScene(scene);
         stage.show();
+
     }
 
     public static void main(String[] args) {
--- src/main/java/com/example/findpho_adminui/HelloController.java
+++ src/main/java/com/example/findpho_adminui/HelloController.java
@@ -1,14 +1,9 @@
 package com.example.findpho_adminui;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
 }
